7b7d2b1ded3f590ff67ef0cbe59025ceb3c6f29a
safdar211ali <safdar211@hotmail.com>
2024-12-26 15:51:36 +0500

    updated

---
+++ buying_house/buying_house/doctype/home_textile_sizes/home_textile_sizes.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, safdar211@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Home Textile Sizes', {
+	// refresh: function(frm) {
+
+	// }
+});
