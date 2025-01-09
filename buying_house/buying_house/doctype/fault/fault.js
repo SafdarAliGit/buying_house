0eed1458278fe99e37e4132a719af1880daef15d
safdar211ali <safdar211@hotmail.com>
2025-01-09 16:05:51 +0500

    updated

---
+++ buying_house/buying_house/doctype/fault/fault.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, safdar211@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Fault', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ buying_house/buying_house/doctype/fault/test_fault.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, safdar211@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFault(FrappeTestCase):
+	pass
