68eace901c79051565beccfd9d480ed8b1671763
SafdarAliGit <safdar211@gmail.com>
2025-04-10 17:21:52 +0500

    update

--- buying_house/buying_house/doctype/inspection_report/inspection_report.js
+++ buying_house/buying_house/doctype/inspection_report/inspection_report.js
@@ -86,13 +86,12 @@ frappe.ui.form.on('Inspection Report', {
     not_finished: function (frm) {
         calculate_not_finished_percent(frm);
     },
-    customer_po: function (frm) {
-        fill_inspection_report_child(frm);
-        if (frm.doc.customer_po) {
+    item: function(frm) {
+        if (frm.doc.item) {
             frappe.call({
-                method: 'buying_house.buying_house.doctype.util.get_item_specification_details_for_inspection_report', // adjust to your method path
+                method: 'buying_house.buying_house.doctype.util.get_item_specification_details', // adjust to your method path
                 args: {
-                    customer_po: frm.doc.customer_po
+                    item_code: frm.doc.item
                 },
                 callback: function(r) {
                     if (r.message) {
@@ -127,6 +126,47 @@ frappe.ui.form.on('Inspection Report', {
             });
         }
     },
+    // customer_po: function (frm) {
+    //     fill_inspection_report_child(frm);
+    //     if (frm.doc.customer_po) {
+    //         frappe.call({
+    //             method: 'buying_house.buying_house.doctype.util.get_item_specification_details_for_inspection_report', // adjust to your method path
+    //             args: {
+    //                 customer_po: frm.doc.customer_po
+    //             },
+    //             callback: function(r) {
+    //                 if (r.message) {
+    //                     // Clear existing rows
+    //                     frm.clear_table('ir_item_spec1');
+    //                     frm.clear_table('ir_item_spec2');
+    //                     frm.clear_table('ir_item_spec3');
+
+    //                     // Populate cpo_item_spec1
+    //                     r.message.item_spec1.forEach(row => {
+    //                         let child = frm.add_child('ir_item_spec1');
+    //                         Object.assign(child, row); // or manually set fields
+    //                     });
+
+    //                     // Populate cpo_item_spec2
+    //                     r.message.item_spec2.forEach(row => {
+    //                         let child = frm.add_child('ir_item_spec2');
+    //                         Object.assign(child, row);
+    //                     });
+
+    //                     // Populate cpo_item_spec3
+    //                     r.message.item_spec3.forEach(row => {
+    //                         let child = frm.add_child('ir_item_spec3');
+    //                         Object.assign(child, row);
+    //                     });
+
+    //                     frm.refresh_field('ir_item_spec1');
+    //                     frm.refresh_field('ir_item_spec2');
+    //                     frm.refresh_field('ir_item_spec3');
+    //                 }
+    //             }
+    //         });
+    //     }
+    // },
     aql_level: function (frm) {
         fetch_min_1_values(frm);
     },
