d9a5d190e64c4144281e4558ec7085035240a2c2
SafdarAliGit <safdar211@gmail.com>
2025-04-16 18:40:32 +0500

    updated

---
+++ buying_house/buying_house/doctype/ir_item_spec1111/ir_item_spec1111.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, safdar211@gmail.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class IRItemSpec1111(Document):
+	pass
